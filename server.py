b2ffde081c59d7f6ff612ebfe7339b61f9a2c7de
steve <e0407222@u.nus.edu>
2022-03-28 22:57:23 +0800

    Add Multithreading

--- server.py
+++ server.py
@@ -1,36 +1,56 @@
 import socket 
 import codecs
+import threading,queue 
+
 HOST = "0.0.0.0"
 PORT = 9999
+q = queue.Queue()
+conn = None
 def header_gen(type,len):
  return "HTTP/1.1 200 OK\r\n Connection: close\r\n Content-Type: {}\r\n Content-Length: {}\r\n\r\n".format(type,len) 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
- s.bind((HOST,PORT))
- s.listen()
- #conn,addr = s.accept() 
- while True:
-  conn,addr = s.accept()
-  type = ""
+
+
+def Producer(s):
+ global conn 
+ while True: 
+  conn,addr=s.accept()
   req = conn.recv(1024).decode()
   print(req)
+  q.put(req) 
+ 
+def Consumer(): 
+ #Consumer 
+ global conn
+ while True:
+  req = q.get(block=True)
+  type = "" 
   if "bio.txt" in req:
    fin=codecs.open("bio.txt","r",encoding="utf-8")
    type = "text/plain"
   elif "favicon" in req:
-   fin=open("favicon.png")
+   fin=open("favicon.ico","rb")
    type = "image/png"
   else: 
-   fin = open("index.html","r",encoding="utf-8")
+   fin = open("index.html","r")
    type = "text/html"
   content1 = fin.read()
   fin.close()
   
-  response = header_gen("text/html",str(len(content1)))+content1 
+  response = header_gen(type,str(len(content1)))+content1 
   conn.send(response.encode("utf-8")) 
-
-  #send http response 
-  #response = "HTTP/1.0 200 OK\n\n"+ content1
-  #conn.sendall(response.encode())
-  conn.close() 
+  #conn.close() 
 
 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+ s.bind((HOST,PORT))
+ s.listen()
+ #conn,addr = s.accept() 
+
+ 
+ t1 = threading.Thread(target=Producer,args=(s,))
+ t2 = threading.Thread(target=Consumer,args=())
+ t1.start()
+ t2.start()
+ t1.join()
+ t2.join()
+ 
